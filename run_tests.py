4570d3acc1f213ed4623a65a6f4d480678f34394
Tizian Zeltner <tizian.zeltner@epfl.ch>
2018-03-14 17:18:35 +0100

    Add pa4 scenes and travis test script

---
+++ run_tests.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import subprocess
+import sys
+
+tests = [
+    "pa4/tests/test-mesh.xml",
+    "pa4/tests/test-mesh-furnace.xml"
+]
+
+total = len(tests)
+passed = 0
+
+failed = []
+
+for t in tests:
+    path = "scenes/" + t
+    ret = subprocess.call(["./build/nori", path])
+    if ret == 0:
+        passed += 1
+    else:
+        failed.append(t)
+
+print("")
+if passed < total:
+    print("\033[91m" + "Passed " + str(passed) + " / " + str(total) + " tests." + "\033[0m")
+    print("Failed tests:")
+    for t in failed:
+        print("\t" + t)
+
+    sys.exit(1)
+else:
+    print("\033[92m" + "Passed " + str(passed) + " / " + str(total) + " tests." + "\033[0m")
+    print("\tNice!")
+
+    sys.exit(0)
